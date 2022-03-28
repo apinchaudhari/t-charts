eab119415d636f159e43406f98391e8aa8aa50f7
Purvi Chaudhari <purvichaudhari@gmail.com>
2022-03-28 22:55:50 +0530

    Added Basic Project setup

---
+++ config/version.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'name'      =>  'Akaunting',
+
+    'code'      =>  'Document',
+
+    'major'     =>  '2',
+
+    'minor'     =>  '1',
+
+    'patch'     =>  '31',
+
+    'build'     =>  '',
+
+    'status'    =>  'Stable',
+
+    'date'      =>  '14-Febrary-2021',
+
+    'time'      =>  '18:00',
+
+    'zone'      =>  'GMT +3',
+
+];
